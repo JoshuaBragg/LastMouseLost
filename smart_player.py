5fb95554978f47c2b1e138a0ddfaeffc62b7ea9b
Joshua <joshua.bragg@outlook.com>
2017-12-16 00:25:53 -0500

    Github Organization

--- smart_player.py
+++ smart_player.py
@@ -1,16 +1,17 @@
 from sys import maxsize
 
+
 class Node:
-	def __init__(self, depth, pnum, board, move = (0, 0), val = 0):
+	def __init__(self, depth, pnum, board, move=(0, 0), val=0):
 		self.depth = depth
 		self.pnum = pnum
 		self.board = board
 		self.val = val
 		self.move = move
 		self.children = []
-		self.CreateChildren()	
-	
-	def CreateChildren(self):
+		self.create_children()
+
+	def create_children(self):
 		if self.depth < 0 or self.val != 0:
 			return
 		# This is much messier than before but cuts down tree size because of the symmetry of the board
@@ -76,15 +77,15 @@ class Node:
 								tempb = self.board.dupe()
 								tempb.update_b(r, a+1)
 								self.children.append(Node(self.depth - 1, -1*self.pnum, tempb, (r, a+1), self.RealVal(self.board)))
-		
-		#if self.depth >= 0 and self.val == 0:
-		#	for r in range(6):
-		#		if not self.board.row_empty(r):
-		#			for a in range(self.board.spot_avail(r)):
-		#				tempb = self.board.dupe()
-		#				tempb.update_b(r, a+1)
-		#				self.children.append(Node(self.depth - 1, -1*self.pnum, tempb, (r, a+1), self.RealVal(self.board)))
-						
+
+		# if self.depth >= 0 and self.val == 0:
+		# 	for r in range(6):
+		# 		if not self.board.row_empty(r):
+		# 			for a in range(self.board.spot_avail(r)):
+		# 				tempb = self.board.dupe()
+		# 				tempb.update_b(r, a+1)
+		# 				self.children.append(Node(self.depth - 1, -1*self.pnum, tempb, (r, a+1), self.RealVal(self.board)))
+
 	def RealVal(self, b):
 		if b.g_o():
 			return maxsize * -1 * self.pnum
@@ -95,14 +96,14 @@ class Node:
 def MinMax(node, depth, pnum):
 	if (depth == 0) or (abs(node.val) == maxsize):
 		return node.val
-	
+
 	bestV = maxsize * -pnum
-	
+
 	for child in node.children:
 		val = MinMax(child, depth - 1, -1 * pnum)
 		if abs(maxsize * pnum - val) < abs(maxsize * pnum - bestV):
 			bestV = val
-			
+
 	return bestV
 
 def goodDepth(b):
